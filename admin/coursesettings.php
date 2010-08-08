1a20607f707a70f9c6c28a2a02e7dfe9f90177b6
jerome mouneyrac <jerome@moodle.com>
2010-08-08 12:49:19 +0000

    hub server MDL-23041 add course main settings (forgot to add a new file)

---
+++ admin/coursesettings.php
@@ -0,0 +1,78 @@
+<?php
+
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// This file is part of Moodle - http://moodle.org/                      //
+// Moodle - Modular Object-Oriented Dynamic Learning Environment         //
+//                                                                       //
+// Moodle is free software: you can redistribute it and/or modify        //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation, either version 3 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// Moodle is distributed in the hope that it will be useful,             //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details.                          //
+//                                                                       //
+// You should have received a copy of the GNU General Public License     //
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.       //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
+/**
+ * On this page administrator can change site settings
+ * @package   localhub
+ * @copyright 2010 Moodle Pty Ltd (http://moodle.com)
+ * @author    Jerome Mouneyrac
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+require('../../../config.php');
+require_once($CFG->libdir . '/adminlib.php');
+require_once($CFG->dirroot . '/local/hub/admin/forms.php');
+
+admin_externalpage_setup('hubcoursesettings');
+
+$id = optional_param('id', 0, PARAM_INT);
+$hub = new local_hub();
+$course = $hub->get_course($id, MUST_EXIST);
+
+//define nav bar
+$PAGE->set_url('/local/hub/admin/coursesettings.php', array('id' => $id));
+$PAGE->navbar->ignore_active(true);
+$PAGE->navbar->add(get_string('administrationsite'));
+$PAGE->navbar->add(get_string('hub', 'local_hub'));
+$PAGE->navbar->add(get_string('managecourses', 'local_hub'),
+        new moodle_url('/local/hub/admin/managecourses.php',
+                array('search' => $course->fullname, 'sesskey' => sesskey())));
+$PAGE->navbar->add(get_string('coursesettings', 'local_hub'),
+        new moodle_url('/local/hub/admin/coursesettings.php', array('id' => $id)));
+
+
+$coursesettingsform = new hub_course_settings_form('',
+        array('id' => $id));
+$fromform = $coursesettingsform->get_data();
+
+//Save settings and redirect to search site page
+if (!empty($fromform)) {    
+    $course->fullname = $fromform->fullname;
+    $course->description = $fromform->description;
+    $course->language = $fromform->language;
+    if (isset($fromform->courseurl)) {
+        $course->courseurl = $fromform->courseurl;
+    } else {
+        $course->demourl = $fromform->demourl;
+    }
+
+    $hub->update_course($course);
+
+    redirect(new moodle_url('/local/hub/admin/managecourses.php',
+            array('coursesettings' => $course->fullname, 'sesskey' => sesskey(),
+                'search' => $course->fullname)));
+}
+
+//OUTPUT
+echo $OUTPUT->header();
+$coursesettingsform->display();
+echo $OUTPUT->footer();
+
