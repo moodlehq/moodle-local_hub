d4cec6711851d68bfff7b3b6b8224243a3501d48
jerome mouneyrac <jerome@moodle.com>
2010-11-10 06:55:32 +0000

    hub server MDL-25039 add send to hub administrator (forgot one new file)

---
+++ db/install.php
@@ -0,0 +1,33 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Hub install
+ * @package   localhub
+ * @copyright 2010 Moodle Pty Ltd (http://moodle.com)
+ * @author    Jerome Mouneyrac
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+//This is temporary till MDL-25115 is implemented
+function xmldb_local_hub_install() {
+    global $DB;
+    $messageprovider = new stdClass();
+    $messageprovider->name = 'coursehubmessage';
+    $messageprovider->component = 'local/hub';
+    $DB->insert_record('message_providers', $messageprovider);
+}
