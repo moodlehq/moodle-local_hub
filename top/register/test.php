664db88001d8e251c9f02700dd19e828633241c8
Petr Skoda <commits@skodak.org>
2011-08-04 22:18:13 +0200

    remove most probably unused registration scripts, it should be handled via hubs, right?

--- top/register/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-die('TODO: Not converted yet.');
-
-require_once('update_list_subscription.php');
-$newemail = 'jordant@blue-ferret.com.au';
-echo update_list_subscription_soap('add', $newemail);
