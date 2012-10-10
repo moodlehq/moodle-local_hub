3b1cbed80413fcfb2a2592f7127d42fded5da9fa
Emess <matt.sharpe@gmail.com>
2012-10-10 14:20:00 +0800

    use HTTPS for Google custom search

--- top/public/search/index.php
+++ top/public/search/index.php
@@ -35,7 +35,7 @@
   var googleSearchDomain = "www.google.com";
   var googleSearchPath = "/cse";
 </script>
-<script type="text/javascript" src="http://www.google.com/afsonline/show_afs_search.js"></script>
+<script type="text/javascript" src="https://www.google.com/afsonline/show_afs_search.js"></script>
 
 
 <?php
