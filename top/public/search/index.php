5f797d347b8070165232065d5e7cbc20982f1b24
Aparup Banerjee <aparup@moodle.com>
2013-10-18 15:27:36 +0800

    MDLSITE-2393 fixed up search with new google custom search engine api.

--- top/public/search/index.php
+++ top/public/search/index.php
@@ -4,7 +4,7 @@
     require_once('../../toplib.php');
 
     $PAGE->set_url('/public/search/');
-    $PAGE->set_context(get_system_context());
+    $PAGE->set_context(context_system::instance());
     $PAGE->set_title('Search moodle.org');
     $PAGE->set_heading($PAGE->title);
     echo $OUTPUT->header();
@@ -12,29 +12,21 @@
     //print_header('Search moodle.org', 'Search moodle.org', 'Search moodle.org');
 
 ?>
-
-<div class="generalbox boxaligncenter" style="text-align:center" >
-
-<form action="https://moodle.org/public/search/" id="cse-search-box">
-  <div>
-    <input type="hidden" name="cx" value="017878793330196534763:-0qxztjngoy" />
-    <input type="hidden" name="cof" value="FORID:9" />
-    <input type="hidden" name="ie" value="UTF-8" />
-    <input type="text" name="q" size="31" />
-    <input type="submit" name="sa" value="Search" />
-  </div>
-</form>
-
-</div>
-
-<div id="cse-search-results"></div>
-<script type="text/javascript">
-  var googleSearchIframeName = "cse-search-results";
-  var googleSearchFormName = "cse-search-box";
-  var googleSearchFrameWidth = ''
-  var googleSearchDomain = "www.google.com";
-  var googleSearchPath = "/cse";
+<div class="generalbox boxaligncenter" style='text-align: center'>
+<script>
+  (function() {
+    var cx = '017878793330196534763:-0qxztjngoy'; //017878793330196534763:-0qxztjngoy
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
 </script>
+<gcse:search></gcse:search>
+</div>
 <script type="text/javascript" src="https://www.google.com/afsonline/show_afs_search.js"></script>
 
 
