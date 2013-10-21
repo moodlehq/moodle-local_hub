bd4865f9adc621b244d123fb02ff5e1457cdcc2d
Aparup Banerjee <aparup@moodle.com>
2013-10-21 14:21:34 +0800

    /public/search/ now forwards params properly, no double encoding

--- top/public/index.php
+++ top/public/index.php
@@ -2,4 +2,9 @@
 
 require('../../../../config.php');
 
-redirect(new moodle_url('/public/search/')); // nothing to see here
+$params['cx'] = optional_param('cx',  '', PARAM_RAW);
+$params['cof'] = optional_param('cof',  '', PARAM_RAW);
+$params['ie'] = optional_param('ie',  '', PARAM_RAW);
+$params['q'] = optional_param('q',  '', PARAM_RAW);
+
+redirect(new moodle_url('/public/search/', $params)); // nothing to see here
