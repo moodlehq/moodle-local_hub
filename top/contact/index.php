10f0b7a32912a3797a5f8c8f5fc06189ab0ce4b2
David Mudrák <david@moodle.com>
2014-01-23 21:25:56 +0100

    Replace the old /contact with a work-in-progress page
    We need this to get rid of the toplib.php usage

--- top/contact/index.php
+++ top/contact/index.php
@@ -1,72 +1,19 @@
 <?php
 
-    require('../../../../config.php');
-    require_once('../toplib.php');
+/**
+ * Provides the content of the moodle.org/contact/ page
+ */
 
-    print_moodle_page_top('contact', '');
+require(__DIR__.'/../../../../config.php');
 
-    $contactintro = array('contactintro');
-    $contactcommunity = array('contactsupporttitle',
-                              'contactsupport',
-                              'http://moodle.org/support/|contactsupporttitle',
-                              'contactmoodlecomtitle',
-                              'contactmoodlecom',
-                              'http://moodle.com/helpdesk/|contactmoodlecomhelpdesk',
-                              'contactbugstitle',
-                              'contactbugs',
-                              'contactsecurity',
-                              'http://tracker.moodle.org/|moodletrackertitle'); /*
-                              
-                              // Contact form removed by Jordan at Martins request on 2011/08/08
-                              'contactmoodleorgtitle',
-                              'contactmoodleorgemail',
-                              '<div class="moodletop link"><span class="arrow sep">►</span> <a href="mailto:s%75%70po%72%74%40%6d%6f%6f%64l%65%2e%6f%72%67">support@moodle.org</a></div>'
-                             );
-    if (isloggedin()) {
-        $contactcommunity[] = 'contactmoodleorgother';
-    } else {
-        $contactcommunity[] = 'contactlogin';
-        $contactcommunity[ ]= 'http://moodle.org/login/|contactmoodleorglogin';
-    }
-    $contactsent = array('contactsent'); */
+$PAGE->set_url(new moodle_url('/contact/index.php'));
+$PAGE->set_context(context_system::instance());
+$PAGE->set_title('Page under construction');
+$PAGE->set_heading($PAGE->title);
 
-    /* This script sends out email : requiring login helps block spam.
-     * TODO : find a way to let anonymous user send out email without opening
-     * the door to spam.
-     */
-    /*
-    if (isloggedin()) {
+$PAGE->navbar->add($PAGE->heading);
 
-        if (($frm = data_submitted()) && confirm_sesskey()) {
+echo $OUTPUT->header();
+echo $OUTPUT->heading($PAGE->heading);
 
-            $userto = $DB->get_record('user', array('id' => 24152), '*', MUST_EXIST);    // helen
-
-            $message = clean_param($frm->contacttext, PARAM_TEXT);
-            $messagesubject = $frm->contactsubject;
-            $messagetext = format_text_email($message, FORMAT_PLAIN) ."\n\n--\nThis message was sent to you via the contact form at http://moodle.org/contact.\n";
-
-            $userfrom = clone($USER);
-            $userfrom->maildisplay = true;
-
-            if (email_to_user($userto, $userfrom, $messagesubject, $messagetext)) {
-                print_moodle_content($contactsent);
-                echo $OUTPUT->continue_button(new moodle_url('/'));
-            } else {
-                $OUTPUT->notify('There was an error while sending out the message. Please try again later.');
-            }
-
-        } else {
-            print_moodle_content($contactintro);
-            print_moodle_content($contactcommunity);
-            echo "<br /><br />";
-
-            include('contact.html');
-        }
-
-    } else  { */
-        print_moodle_content($contactintro);
-        print_moodle_content($contactcommunity);
- // }
-
-
-    print_moodle_page_bottom('contact');
+echo $OUTPUT->footer();
