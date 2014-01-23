5199ce0fba624e9558d5f7d2e4aaa54a650a74ea
David Mudrák <david@moodle.com>
2014-01-23 21:37:41 +0100

    Overall cleanup of legacy and not used parts

--- cli/corecliupgrade.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/php -r "posix_setuid('48'); require_once('/var/www/vhosts/moodle.org/html/admin/cli/upgrade.php');" -- --non-interactive
--- cli/purgecaches.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script purges all caches, we need to run this after an upgrade when we change things like local/plugins
-# This script is suitable to be run via cron if required.
-/usr/bin/php -r "posix_setuid('48'); define('CLI_SCRIPT', true); require_once('/var/www/vhosts/moodle.org/html/config.php'); require_once(\$CFG->libdir.'/adminlib.php'); purge_all_caches();"
