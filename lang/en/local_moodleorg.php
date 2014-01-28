f34e5ae61087629bac730cc370eccd3d084891da
David Mudrák <david@moodle.com>
2014-01-28 17:01:58 +0100

    Add Moodle standards URL

--- lang/en/local_moodleorg.php
+++ lang/en/local_moodleorg.php
@@ -274,4 +274,5 @@ $string['top10sitesbycourses'] = 'Top 10 sites by courses';
 $string['top10countriesbyregistration'] = 'Top 10 countries by registrations';
 $string['versionsused'] = 'Versions used';
 $string['url-about'] = 'http://docs.moodle.org/en/About_Moodle';
+$string['url-standards'] = 'http://docs.moodle.org/en/Standards';
 $string['usingmoodle'] = 'Using Moodle';
