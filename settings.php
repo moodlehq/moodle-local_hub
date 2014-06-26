3703b5211763d256ec177a53fda69c0d1782cf20
Aparup Banerjee <aparup@moodle.com>
2014-06-26 10:19:11 +0800

    (final) removed other local_moodleorg stuff in prep to merge into local_hub

--- settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-
-if (has_capability('moodle/site:config', context_system::instance())) {
-    $ADMIN->add('localplugins', new admin_externalpage('local_moodleorg_coursemapping', 'Moodle.org course mapping', '/local/moodleorg/admin/coursemapping.php', 'moodle/site:config'));
-}
